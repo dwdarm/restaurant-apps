fccf2b2cb207270bd6f922dfc880cbfbff4884ff
Fajar <dwdarm95@gmail.com>
2021-11-11 13:34:41 +0700

    update bundler analyzer setting

--- webpack.prod.js
+++ webpack.prod.js
@@ -54,6 +54,9 @@ module.exports = merge(common, {
     ],
   },
   plugins: [
-    new BundleAnalyzerPlugin(),
+    new BundleAnalyzerPlugin({
+      analyzerMode: 'static',
+      openAnalyzer: false,
+    }),
   ],
 });
