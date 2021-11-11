c7e43c4a6db4951aeaf1ac95081b47ed8fbc44dd
Fajar <dwdarm95@gmail.com>
2021-11-11 13:55:20 +0700

    fix sw.js

--- src/scripts/sw.js
+++ src/scripts/sw.js
@@ -1,8 +1,11 @@
-// eslint-disable-next-line import/no-extraneous-dependencies
+// eslint-disable-next-line no-undef
+importScripts('https://unpkg.com/regenerator-runtime@0.13.7/runtime.js');
+
+// eslint-disable-next-line import/no-extraneous-dependencies, import/first
 import { precacheAndRoute } from 'workbox-precaching';
-// eslint-disable-next-line import/no-extraneous-dependencies
+// eslint-disable-next-line import/no-extraneous-dependencies, import/first
 import { registerRoute, setDefaultHandler } from 'workbox-routing';
-// eslint-disable-next-line import/no-extraneous-dependencies
+// eslint-disable-next-line import/no-extraneous-dependencies, import/first
 import { CacheFirst, NetworkFirst } from 'workbox-strategies';
 
 // eslint-disable-next-line no-restricted-globals
